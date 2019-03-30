1755ec3f541a92ee7e0e30b46afdac3c15636969
JaganathJoshua <jaganath.joshua@freshworks.com>
2019-03-30 05:40:57 +0530

    Learning GO pkg structures: Created a pkg greeter and moved printWelcomeMsg() function to Greet() in greeter package and wrote the corresponding test file.

---
+++ src/greeter/greeter.go
@@ -0,0 +1,8 @@
+package greeter
+
+import "fmt"
+
+// function to greet users to GO lang
+func Greet() {
+	fmt.Println("Welcome to GO world!")
+}
--- src/greeter/greeter_test.go
+++ src/greeter/greeter_test.go
@@ -1,13 +1,15 @@
 // Unit Tests for go-world.go functions
 
-package main
+package greeter_test
+
+import "greeter"
 
 // In GO any function prefixed with Example will be excuted as test and its output
 // is asserted against the Output comment in the function. Its an useful option to
 // test the functions which just prints
-func ExamplePrintWelcomeMsg() {
+func ExampleGreet() {
 	// calling the function to be tested
-	printWelcomeMsg()
+	greeter.Greet()
 	// In Go, to test the functions which simply prints,
 	// we can use the below output comment which acts as assertion within Example functions
 
